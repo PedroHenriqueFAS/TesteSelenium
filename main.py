bb436663d1222233257290859713375f1211453d
Pedro <pedrohenriquefa97@gmail.com>
2025-06-03 17:46:09 -0300

    Update chromedriver path to use current working directory

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import os
+path = os.getcwd() + '/chromedriver.exe'
 
 options = webdriver.ChromeOptions()
 
-service = Service(executable_path = 'chromedriver.exe')
+service = Service(executable_path = path)
 
 driver = webdriver.Chrome(options=options, service=service)
 
